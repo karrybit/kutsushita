2550c4d14eb7da8f1324688b822196d85f7e717d
makanai5610 <takumi.k.5610@gmail.com>
2020-09-08 22:00:12 +0900

    Add main.go

---
+++ catalogue/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const ServiceName = "catalogue"
+
+func main() {
+	// ctx := context.Background()
+
+	// TODO: opentracing
+
+	// TODO: db
+
+	// TODO: service
+
+	// TODO: launch server
+
+	errc := make(chan error)
+
+	go func() {
+		errc <- http.ListenAndServe(":80", nil)
+	}()
+
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errc <- fmt.Errorf("%s", <-c)
+	}()
+
+	fmt.Println("exit", <-errc)
+}
