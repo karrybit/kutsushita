2f070d9119383896ed6f6e726b21c87fd164098f
makanai5610 <takumi.k.5610@gmail.com>
2020-11-19 17:27:43 +0900

    add

--- user/api/transport.go
+++ user/api/transport.go
@@ -6,6 +6,9 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/zap"
+
+	"user/db"
+	"user/users"
 )
 
 var ErrInvalidRequest = errors.New("Invalid request")
@@ -13,7 +16,7 @@ var ErrInvalidRequest = errors.New("Invalid request")
 func MakeHTTPHandler(service Service, logger *zap.Logger /*, tracer */) *fiber.App {
 	app := fiber.New()
 	app.Post("/register", register(service))
-	app.Get("/customers", register(service))
+	app.Get("/customers", customers(service))
 	app.Get("/addresses", register(service))
 	app.Get("/cards", register(service))
 	app.Post("/customers", register(service))
@@ -35,11 +38,46 @@ func register(service Service) func(c *fiber.Ctx) error {
 		if err != nil {
 			return err
 		}
-		postResponse := postResponse{ID: id}
-		b, err := json.Marshal(postResponse)
+		return c.JSON(postResponse{ID: id})
+	}
+}
+
+func customers(service Service) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		ctx := c.Context()
+		req := new(GetRequest)
+		if err := json.Unmarshal(c.Body(), &req); err != nil {
+			return err
+		}
+		if req.ID == "" {
+			return c.JSON(EmbedStruct{addressesResponse{Addresses: make([]users.Address, 0)}})
+		}
+
+		us, err := service.GetUsers(ctx, req.ID)
 		if err != nil {
 			return err
 		}
-		return c.Send(b)
+		if len(us) == 0 {
+			if req.Attr == "addresses" {
+				return c.JSON(EmbedStruct{addressesResponse{Addresses: make([]users.Address, 0)}})
+			}
+			if req.Attr == "cards" {
+				return c.JSON(EmbedStruct{addressesResponse{Addresses: make([]users.Address, 0)}})
+			}
+			return c.JSON(users.User{})
+		}
+
+		user := us[0]
+		if err := db.GetUserAttributes(ctx, &user); err != nil {
+			return err
+		}
+		if req.Attr == "address" {
+			return c.JSON(EmbedStruct{addressesResponse{Addresses: user.Addresses}})
+		}
+		if req.Attr == "cards" {
+			return c.JSON(EmbedStruct{cardsResponse{Cards: user.Cards}})
+		}
+
+		return c.JSON(user)
 	}
 }
